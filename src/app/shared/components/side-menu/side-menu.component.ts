dbc392b9fa13b0d265d8c103dcb575e28b17bd6b
Orlando YV <sergioyv@gmail.com>
2025-03-22 12:32:20 -0500

    OY - first files

---
+++ src/app/shared/components/side-menu/side-menu.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-menu',
+  imports: [],
+  templateUrl: './side-menu.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SideMenuComponent { }
